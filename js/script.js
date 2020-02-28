b7dae2a97858f80bbe63a8491b73feb82b8dc67e
Diegochits <54562062+dhna32@users.noreply.github.com>
2020-02-27 19:43:07 -0600

    Finished script.js
    - Added comments
    - Script working succesfully
    - Ready to send

--- js/script.js
+++ js/script.js
@@ -7,21 +7,20 @@ by: Diego Hernández 27/02/2019
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-
-const listItem = document.querySelectorAll('li');
-//console.log(listItem); 
-const itemsPerPage = 10;
+/*Global variables needed to control properly our program */
+const listItem = document.querySelectorAll('li'); //selects all the students from the provided list
+const itemsPerPage = 10; 
 
 
 
 const showPage = (list, page) => {
-
+  /*Basic math used for determine the range of the students list to show*/
   const startIndex = (page * itemsPerPage) - itemsPerPage;
   const endOfIndex = (page * itemsPerPage)-1;
-
+  /*Loop to determine which section of students are gonna be shown*/ 
   for (let i = 0; i < list.length; i++) {
     if ([i] >= startIndex && [i] <= endOfIndex ) {
-       listItem[i].style.display = '';
+      listItem[i].style.display = '';
     } else {
       listItem[i].style.display = 'none';
     }
@@ -29,14 +28,9 @@ const showPage = (list, page) => {
 }
 
 
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
-
 const appendPageLinks = (list) => {
-  const pagesNeeded = Math.ceil(listItem.length/itemsPerPage);
-
+  const pagesNeeded = Math.ceil(listItem.length/itemsPerPage); // Basic Math used to determine the "pages" needed according to the students list
+  /*DOM Tree Available on README.ms*/ 
   const div = document.createElement('div');
   div.className = 'pagination';
   const divPage = document.querySelector('.page');
@@ -45,6 +39,7 @@ const appendPageLinks = (list) => {
   const ul = document.createElement('ul');
   div.appendChild(ul);
   
+  /*Loop that creates the links required depending on the list of students */
   for (let i = 0; i < pagesNeeded; i++) {
     const li = document.createElement('li');
     const a = document.createElement('a');
@@ -53,20 +48,23 @@ const appendPageLinks = (list) => {
     li.appendChild(a);
     ul.appendChild(li); 
 
-    a.addEventListener('click', function(event) {  
-      let anchorArray = document.getElementsByTagName('a');
-      for (let index = 0; index < anchorArray.length; index++) {
-        anchorArray[i].className = '';
-      }
+    let linksArray = document.getElementsByTagName('a'); //Gathered the 'a' tags into a single variable
 
-      event.target.className = 'active';
-      showPage(listItem, a.innerHTML);
-    });
+    for (let index = 0; index < linksArray.length; index++) {  //loop to add e.listeners to the anchor tags
+      linksArray[index].addEventListener('click', function(event) {
+        for (let j = 0; j < linksArray.length; j++) { //inner loop that goes through all the anchor tags and removes the class
+            linksArray[j].className = '';
+        }
+        event.target.className = 'active'; //sets the anchor tag class that was triggered to active 
+        showPage(listItem, linksArray[index].textContent);  //show the selected range of students selected     
+      });
+    }
   }
   
 }
 
-showPage(listItem, 1)
+//function calls to make it work :)
+showPage(listItem, 1);
 appendPageLinks(listItem);
 
 
